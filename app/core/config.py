4f3afd8fe0042b304b0c18a7499c63862c279b09
Alexander Kim <limosha@inbox.ru>
2026-02-23 18:29:34 +0300

    ci(build): fix settings parameters in config.py and .env.example

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -7,16 +9,15 @@ class Settings(BaseSettings):
         env_file=".env",
         env_file_encoding="utf-8",
         case_sensitive=False,
+        extra="forbid",
     )
-
+    database_url: str = Field(..., validation_alias="DATABASE_URL")
+    app_port: int
+    log_level: str
+    debug: bool = False
+    cors_origins: List[str] = ["*"]
+    parse_schedule_minutes: int
     external_vacancies_api_url: str
-
-    database_url: str = Field(
-        "postgresql+asyncpg://postgres:postgres@db:5432/postgres",
-        validation_alias="DATABASE_URL",
-    )
-    log_level: str = "INFO"
-    parse_schedule_minutes: int = 5
 
 
 settings = Settings()
